90f322e21d01334a9dea614772f4f1be62c9d5ff
Vitalii <flipsun@gmail.com>
2017-05-18 13:36:16 +0300

    Paddle

--- src/Pong.java
+++ src/Pong.java
@@ -9,16 +9,19 @@ import java.awt.event.KeyListener;
 public class Pong extends Applet implements Runnable, KeyListener{
     final int WIDTH =700, HEIGHT = 500;
     Thread thread;
+    HumanPaddle p1;
     
     public void init(){
         this.resize(WIDTH,HEIGHT);
         this.addKeyListener(this);
+        p1= new HumanPaddle(2);
         Thread thread = new Thread(this);
         thread.start();
     }
     public void paint(Graphics g){
         g.setColor(Color.black);
         g.fillRect(0,0,WIDTH,HEIGHT);
+        p1.draw(g);
     }
     public void update(Graphics g){
         paint(g);
@@ -28,6 +31,7 @@ public class Pong extends Applet implements Runnable, KeyListener{
     public void run() {
         for(;;){
 
+            p1.move();
             repaint();
             try {
                 Thread.sleep(10);
@@ -45,20 +49,20 @@ public class Pong extends Applet implements Runnable, KeyListener{
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode()== KeyEvent.VK_UP){
-            System.out.println("3");
+            p1.setUpAccel(true);
         }
         else if(e.getKeyCode()== KeyEvent.VK_DOWN){
-            System.out.println("4");
+            p1.setDownAccel(true);
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         if(e.getKeyCode()== KeyEvent.VK_UP){
-            System.out.println("1");
+            p1.setUpAccel(false);
         }
         else if(e.getKeyCode()== KeyEvent.VK_DOWN){
-            System.out.println("2");
+            p1.setDownAccel(false);
         }
     }
 }
